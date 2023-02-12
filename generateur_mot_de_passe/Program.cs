e4a10a1ba99a530925c3cf27c3bcdf04305f8ce0
ADACH <ludovic.adach@sogeti.com>
2023-02-12 11:18:28 +0100

    project demo password

---
+++ generateur_mot_de_passe/Program.cs
@@ -0,0 +1,33 @@
+﻿using formationCS;
+using System;
+using System.Collections.Generic;
+
+namespace generateur_mot_de_passe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Demander a longueur du mot de passe (DemanderNombre) int longeur_mot_de_passe = ...
+            int longeurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe : ");
+
+            // 2 - Poser la question du choix du type de mot de passe et récupérer le choix
+            int choix = outils.DemanderNombreEntre("Vous voulez un mot de passe avec:\n" +
+                "1 - Uniquement des caractères en minuscule\n" +
+                "2 - Des caractères minuscules et majuscules\n" +
+                "3 - Des caractères et des chiffres\n" +
+                "4 - Caractères, chiffres et caractères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+            int nombreMotDePasse = outils.DemanderNombrePositifNonNul("Combien de mot de passe ? : ");
+
+            List<MotDePasse> reponseGenerateur = outils.GenerateurAllMDP(longeurMotDePasse, choix, nombreMotDePasse);
+
+            //afficher les résultats
+            reponseGenerateur.ForEach(motDepasse =>
+            {
+                Console.WriteLine(motDepasse.Name);
+            });
+        }
+    }
+}
---
+++ generateur_mot_de_passe/outils.cs
@@ -0,0 +1,104 @@
+﻿using generateur_mot_de_passe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            // nombre = DemanderNombre(question)
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                // valide
+                return nombre;
+            }
+            Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
+
+            return DemanderNombreEntre(question, min, max);
+        }
+
+        static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                }
+            }
+        }
+
+        public static MotDePasse GenerationMDP(int longeurMotDePasse, int choix)
+        {
+            //creation des possibilitées
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caracteres = "&#(?[!-])@$";
+            string alphabet ="";
+
+            switch (choix)
+            {
+                case 1:
+                    alphabet = minuscules;
+                    break;
+
+                case 2:
+                    alphabet = minuscules + majuscules;
+                    break;
+
+                case 3:
+                    alphabet = chiffres + caracteres;
+                    break;
+
+                case 4:
+                    alphabet = minuscules + majuscules + chiffres + caracteres;
+                    break;
+            }
+
+            string motDePasse = "";
+            int longueurAlphabet = alphabet.Length;
+            Random rand = new Random();
+
+            //boucler sur longueur motdepasse
+            for (int i = 0; i < longeurMotDePasse; i++)
+            {
+                int index = rand.Next(0, longueurAlphabet);
+                motDePasse += alphabet[index];
+            }
+
+            MotDePasse mot = new MotDePasse();
+            mot.Name = motDePasse;
+            
+            return mot;
+        }
+
+        public static List<MotDePasse> GenerateurAllMDP(int longeurMotDePasse, int choix, int nombreMotDePasse)
+        {
+            List<MotDePasse> list = new List<MotDePasse>();
+            for(int i= 0; i < nombreMotDePasse; i++)
+            {
+                list.Add(GenerationMDP(longeurMotDePasse, choix));
+            }
+            return list;
+        }
+    }
+}
